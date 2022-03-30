a91c48d3bb3dc37d415af31adfbf2028d6a76698
A-20-21-4b <102694932+A-20-21-4b@users.noreply.github.com>
2022-03-31 00:19:07 +0300

    Add files via upload

---
+++ uchprak/uchprak.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    const int n = 10;
+    int Massiv[n];
+    int var;
+    cout << "Введите " << n << " значений:" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> Massiv[i];
+    }
+    cout << "Введите значение для поиска:" << endl; cin >> var;
+    for (int i = 0; i < n; i++) {
+        if (Massiv[i] == var) {
+            cout << "Найдено значение " << Massiv[i] << " под номером " << i + 1 << endl;
+        }
+    }
+}
